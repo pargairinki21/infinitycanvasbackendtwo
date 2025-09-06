9bfbb85b4bdc0b7c58d6941a2d84389ec7edbb7a
Rinki Pargai <rinkisinghpargai@gmail.com>
2025-09-06 14:58:22 +0530

    new message

--- main.py
+++ main.py
@@ -174,4 +174,6 @@ def take_picture():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8002)
+    import os
+    port = int(os.environ.get("PORT", 8002))
+    uvicorn.run(app, host="0.0.0.0", port=port)
